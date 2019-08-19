619b090f1185118dfb5868ac1eb373415de61b6a
galberding <galberding@techfak.uni-bielefeld.de>
2019-08-19 08:45:54 +0200

    Setting default iterations for gridsearch to 100 and error to 5, also increase the batchsize to 30

--- grid_search.py
+++ grid_search.py
@@ -19,11 +19,11 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Train the network for the given data.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-m", "--model", nargs=1, metavar="<pen|sphere|qube>", required=True, type=str)
-    parser.add_argument("-z", "--z_dim", nargs=1, default=[7, 8, 9, 10, 11, 12, 13, 14], type=int, help="Set the dimension of the latent space")
-    parser.add_argument("-i", "--max_iterations", nargs=1, default=[350], type=int, help="Set max epoch iteration")
+    parser.add_argument("-z", "--z_dim", nargs=1, default=[10, 11, 12, 13, 14], type=int, help="Set the dimension of the latent space")
+    parser.add_argument("-i", "--max_iterations", nargs=1, default=[100], type=int, help="Set max epoch iteration")
     parser.add_argument("-o", "--output_dir", nargs=1, default=[''], type=str, help="Set output dir")
-    parser.add_argument("-b", "--batch", nargs=1, default=[25], type=int, help="Batchsize")
-    parser.add_argument("-e", "--error", nargs=1, default=[30], type=int, help="Quit training when error is reached!")
+    parser.add_argument("-b", "--batch", nargs=1, default=[30], type=int, help="Batchsize")
+    parser.add_argument("-e", "--error", nargs=1, default=[5], type=int, help="Quit training when error is reached!")
 
     args = parser.parse_args()
 
